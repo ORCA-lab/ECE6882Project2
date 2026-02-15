1cce072c478126a74d76c8d9f171db14c96cb01e
IrvineZou <wenchengz@email.gwu.edu>
2026-02-15 13:47:23 -0500

    Update evaluation.py

--- LunarLander-v3/evaluation.py
+++ LunarLander-v3/evaluation.py
@@ -3,92 +3,64 @@ import gymnasium as gym
 import torch
 import numpy as np
 import imageio.v2 as imageio
-from LunarAgent import DQNAgent
-
-
-def make_env(render_mode=None, enable_wind=False, gravity=-10.0,
-             wind_power=15.0, turbulence_power=1.5):
-    env = gym.make(
-        "LunarLander-v3",
-        render_mode=render_mode,
-        continuous=False,
-        gravity=gravity,
-        enable_wind=enable_wind,
-        wind_power=wind_power,
-        turbulence_power=turbulence_power,
-    )
-    return env
+#from LunarAgent import DQNAgent
+from xxxAgent import make_env,xxxAgent
 
 def evaluate(
-    qfile: str,
-    seeds=(0, 2),
-    max_steps: int = 1200,
-    render: bool = True,
-    wind_test: bool = True,
-    device: str = "cuda" if torch.cuda.is_available() else "cpu",
+    agent,
+    env,
+    testcase=[0, 2],
 ):
+    """
+    Evaluates a saved agent on multiple seeds.
+    If wind_test=True, we enable wind (harder) during eval.
+    """
     results = []
 
-    # Create an env once just to read dims
-    tmp_env = make_env(render_mode=None, enable_wind=False)
-    obs_dim = int(np.prod(tmp_env.observation_space.shape))
-    n_actions = int(tmp_env.action_space.n)
-    tmp_env.close()
-
-    #Todo: set your own agent
-    agent = LunarLanderAgent()
-    agent.load_parameter(qfile)
-
-    for i, seed in enumerate(seeds, start=1):
-        env = make_env(render_mode="rgb_array" if render else None, enable_wind=wind_test)
+    for i, seed in enumerate(testcase, start=1):
+        #env = make_env(render_mode="rgb_array" if render else None, enable_wind=wind_test)
         frames=[]
         s, _ = env.reset(seed=int(seed))
         ep_ret = 0.0
         ep_len = 0
-
-
-        angle_sum = 0.0
-        angvel_sum = 0.0
-
-        for _ in range(max_steps):
+        while True:
             frames.append(env.render())
-            #Todo: set your agents' action selection function
-            a = agent.act(s)
+            a = agent.act(s, greedy=True)
             s, r, terminated, truncated, info = env.step(a)
 
             ep_ret += float(r)
             ep_len += 1
-
-            # obs = [x, y, vx, vy, angle, angularVel, leftLeg, rightLeg]
-            angle_sum += float(abs(s[4]))
-            angvel_sum += float(abs(s[5]))
-
             if terminated or truncated:
                 break
 
         env.close()
 
-        # A simple "robustness-adjusted" score (optional)
-        # Lower angle/angvel is better.
-        imageio.mimsave("./sample_testcase/testcase_{}.gif".format(seed), frames, fps=30)
-        adjusted = ep_ret - 0.01 * (angle_sum + angvel_sum)
+        imageio.mimsave("testcase_{}.gif".format(seed), frames, fps=30)
+        adjusted = ep_ret #- 0.01 * (angle_sum + angvel_sum)
 
-        print(f"[Eval {i}] | score={adjusted:8.2f}")
+        print(
+            f"[Eval {i}] seed={seed:>3} | score={adjusted:8.2f}"
+        )
 
-        results.append(adjusted)
+        results.append(ep_ret)
 
     return results
 
 
-
 if __name__ == "__main__":
-    qfile = "q.pt"
-
-    # Evaluate
+    # Define your environment
+    # example
+    env = make_env(render_mode="rgb_array",enable_wind=True)
+    #initialize your agent
+    #example
+    obs_dim = int(np.prod(env.observation_space.shape))
+    n_actions = int(env.action_space.n)
+    agent = xxxAgent(state_dim=obs_dim, n_actions=n_actions)
+    agent.load_parameter("xx.pt")
+    #test the agent
+    testcases=[0,2]
     _ = evaluate(
-        qfile=qfile,
-        seeds=(0, 2),
-        max_steps=1200,
-        render=True,
-        wind_test=True,   # stress test with wind
+        agent,
+        env,
+        testcase=testcases
     )
